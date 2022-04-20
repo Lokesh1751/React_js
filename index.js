9c82fa93360e5ffec00427e54c94863528a8194d
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-21 00:50:41 +0530

    new commit

--- index.js
+++
@@ -1,9 +0,0 @@
-ReactDOM.render(<h1>Hello, everyone</h1>,document.getElementById("root"))
-ReactDOM.render(<ul>
-    <li>Thing 1</li>
-    <l1>Thing 2</l1> 
-    <li>Thing 3</li>
-    </ul>
-    ,document.getElementById("root2"))
-
-    
