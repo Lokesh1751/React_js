8b428a721c9ec4f0741a8cfc69b0632a83e78d64
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-23 22:45:05 +0530

    new commit

---
+++ Project1/App.js
@@ -0,0 +1,20 @@
+
+function Main(){
+    return(
+        <h1> hello</h1>
+    )
+}
+function Nav(){
+    return(
+        <h1> hello</h1>
+    )
+}
+ function App(){
+    return(
+        <div>
+            <Nav/>
+            <Main/>
+        </div>
+    )
+}
+ReactDOM.render(<App/>,document.getElementById("root"))
