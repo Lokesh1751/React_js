4184ba370ac262c7a3eb427b8d451a95e8848696
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-24 16:47:11 +0530

    new commit

--- Project1/App.js
+++ Project1/App.js
@@ -1,20 +1,33 @@
-function Main(){
+function Main() {
+  return (
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
+       <li>was first released in 2013</li>
+       <li> Was orignallt created by Jordan Walke</li>
+       <li>Has well over 100K stars on GitHub</li>
+       <li>Is maintained by Facebook</li>
+       <li>Powers thousands of enterprise apps,including mobile apps</li>
 
-    return(
-        <h1>Main Component</h1>
-    )
+      </ul>
+    </main>
+  );
 }
-function Nav(){ 
-    return(
-        <h1> Nav Component</h1>
-    )
+function Nav() {
+  return (
+    <nav className="navv">
+      <img src="logo.jpg" alt="" height="40px" />
+      <h3>ReactFacts</h3>
+      <h4>React Course-Project 1</h4>
+    </nav>
+  );
 }
- function App(){
-    return(
-        <div>
-            <Nav/>
-            <Main/>
-        </div>
-    )
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
 }
-ReactDOM.render(<App/>,document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
