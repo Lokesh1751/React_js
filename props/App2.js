d8336ab7732fb5e4b84250b047543b833dbe871e
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-29 22:33:18 +0530

    props

--- props/App2.js
+++ props/App2.js
@@ -2,11 +2,12 @@ function Little(props) {
     console.log(props) // 4 objets shown
   return (
     <div className="contact-card">
-      <img src="cat2.jpg" width="200px" />
+      <img src={props.img} width="200px" />
       <div>
-      <h3>{props.name}</h3>
+      <h3 className="namee">{props.name}</h3>
       </div>
       <p>{props.phn}</p>
+      <p>{props.email}</p>
     </div>
   );
 }
@@ -14,9 +15,9 @@ function Ans() {
   return (
     <div className="divv">
       <Little name="Sujal Angi" img="cat2.jpg" phn="8571832981" email="sujalangi@gmail.com" />
-      <Little name="Mohak " img="cat3.jpg" phn="8673638282"  email="sujalangi@gmail.com" />
-      <Little name="Abhishek" img="cat4.webp" phn="8974362710" email="sujalangi@gmail.com"  />
-      <Little name="Mehar" img="cat5.jpg"  phn="9732671095" email="sujalangi@gmail.com" />
+      <Little name="Mohak " img="cat3.jpg" phn="8673638282"  email="mohak@gmail.com" />
+      <Little name="Abhishek" img="cat4.webp" phn="8974362710" email="abhishek@gmail.com"  />
+      <Little name="Mehar" img="cat5.jpg"  phn="9732671095" email="mehar@gmail.com" />
     </div>
   );
 }
