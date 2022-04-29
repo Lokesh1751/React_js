e613f26da557a616e865cd085b6894dbce64de76
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-29 22:38:20 +0530

    passing arguments

---
+++ props/App3.js
@@ -0,0 +1,24 @@
+function Little({img,name,phone,email}) {
+    // console.log(props) // 4 objets shown
+  return (
+    <div className="contact-card">
+      <img src={img} width="200px" />
+      <div>
+      <h3 className="namee">{name}</h3>
+      </div>
+      <p>{phone}</p>
+      <p>{email}</p>
+    </div>
+  );
+}
+function Ans() {
+  return (
+    <div className="divv">
+      <Little name="Sujal Angi" img="cat2.jpg" phn="8571832981" email="sujalangi@gmail.com" />
+      <Little name="Mohak " img="cat3.jpg" phn="8673638282"  email="mohak@gmail.com" />
+      <Little name="Abhishek" img="cat4.webp" phn="8974362710" email="abhishek@gmail.com"  />
+      <Little name="Mehar" img="cat5.jpg"  phn="9732671095" email="mehar@gmail.com" />
+    </div>
+  );
+}
+ReactDOM.render(<Ans />, document.getElementById("root"));
