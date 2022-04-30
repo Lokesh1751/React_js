dfd7547dbebdd179fcb6f84563256b0ad19332b0
lokesh13kumar2003 <lokesh1751.be21@chitkara.edu.in>
2022-04-30 14:39:13 +0530

    changes done

--- props/App2.js
+++ props/App2.js
@@ -1,5 +1,5 @@
 function Little(props) {
-    console.log(props) // 4 objets shown
+    console.log(props.isGood) // 4 objets shown
   return (
     <div className="contact-card">
       <img src={props.img} width="200px" />
@@ -8,16 +8,44 @@ function Little(props) {
       </div>
       <p>{props.phn}</p>
       <p>{props.email}</p>
+      <p>{props.upvotes}</p>
     </div>
   );
 }
 function Ans() {
   return (
     <div className="divv">
-      <Little name="Sujal Angi" img="cat2.jpg" phn="8571832981" email="sujalangi@gmail.com" />
-      <Little name="Mohak " img="cat3.jpg" phn="8673638282"  email="mohak@gmail.com" />
-      <Little name="Abhishek" img="cat4.webp" phn="8974362710" email="abhishek@gmail.com"  />
-      <Little name="Mehar" img="cat5.jpg"  phn="9732671095" email="mehar@gmail.com" />
+      <Little name="Sujal Angi" img="cat2.jpg" phn="8571832981" email="sujalangi@gmail.com"
+      isGood={true}
+      upvotes={10}
+      downvotes={9}
+      comments={[{author:"sujal",body:" ",title:"info"}]}
+
+      
+      />
+      <Little name="Mohak " img="cat3.jpg" phn="8673638282"  email="mohak@gmail.com"
+       isGood={true}
+       upvotes={10}
+       downvotes={9}
+       comments={[{author:"mohak",body:" ",title:"info"}]}
+      
+      />
+      <Little name="Abhishek" img="cat4.webp" phn="8974362710" email="abhishek@gmail.com" 
+       isGood={false}
+       upvotes={10}
+       downvotes={9}
+       comments={[{author:"abhishek",body:" ",title:"info"}]}
+
+      
+      />
+      <Little name="Mehar" img="cat5.jpg"  phn="9732671095" email="mehar@gmail.com"
+       isGood={true}
+       upvotes={10}
+       downvotes={9}
+       comments={[{author:"mehar",body:" ",title:"info"}]}
+
+      
+      />
     </div>
   );
 }
